82c2fda6dc936d9e8ed560348015ad3ab6a7d23f
tph7897 <132332533+tph7897@users.noreply.github.com>
2024-03-05 17:51:31 +0900

    [Feat] 회원가입 페이지 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./_components/ModeToggle";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >{children}</ThemeProvider></body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+          <ModeToggle />
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,10 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { ModeToggle } from "@/app/_components/ModeToggle";
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 const SchemaCheck = z.object({
   username: z.string().min(2, { message: "이름은 2글자 이상이어야 합니다." }).max(100, { message: "이름은 100글자 이하이어야 합니다." }),
@@ -19,9 +22,21 @@ const SchemaCheck = z.object({
     .max(11, "연락처는 11자리여야 합니다.")
     .refine((value) => /^010\d{8}$/.test(value), "010으로 시작하는 11자리 숫자를 입력해주세요"),
   role: z.string().min(2, { message: "역할을 선택해주세요." }),
+  password: z
+    .string()
+    .min(6, "비밀번호는 최소 6자리 이상이어야 합니다.")
+    .max(100, "비밀번호는 100자리 이하이어야 합니다.")
+    .refine((value) => /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value), "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다."),
+  confirmPassword: z
+    .string()
+    .min(6, "비밀번호는 최소 6자리 이상이어야 합니다.")
+    .max(100, "비밀번호는 100자리 이하이어야 합니다.")
+    .refine((value) => /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value), "비밀번호는 최소 6자리 이상, 영문, 숫자, 특수문자를 포함해야 합니다."),
 });
 
 export default function Home() {
+  const [step, setStep] = useState<number>(0);
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof SchemaCheck>>({
     resolver: zodResolver(SchemaCheck),
     defaultValues: {
@@ -29,25 +44,35 @@ export default function Home() {
       email: "",
       phone: "",
       role: "",
+      password: "",
+      confirmPassword: "",
     },
   });
 
   function onSubmit(values: z.infer<typeof SchemaCheck>) {
-    console.log(values);
+    const { password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      toast({
+        title: "비밀번호가 일치하지 않습니다.",
+        variant: "destructive",
+        duration: 1000,
+      });
+      return;
+    }
+    alert(JSON.stringify(values, null, 4));
   }
 
   return (
-    <>
-      <ModeToggle />
-      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <Card className="w-[380px]">
-          <CardHeader>
-            <CardTitle>계정을 생성합니다</CardTitle>
-            <CardDescription>필수 정보를 입력해볼게요</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="relative space-y-3 overflow-x-hidden">
+    <main className="flex h-screen flex-col items-center justify-between p-52">
+      <Card className="w-[380px]">
+        <CardHeader>
+          <CardTitle>계정을 생성합니다</CardTitle>
+          <CardDescription>필수 정보를 입력해볼게요</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="relative space-y-3 overflow-x-hidden">
+              <motion.div className={cn("space-y-3")} animate={{ translateX: `${step * -100}%` }} transition={{ ease: "easeInOut" }}>
                 <FormField
                   control={form.control}
                   name="username"
@@ -108,12 +133,75 @@ export default function Home() {
                     </FormItem>
                   )}
                 />
-                <Button type="submit">다음 단계로 ⭢</Button>
-              </form>
-            </Form>
-          </CardContent>
-        </Card>
-      </div>
-    </>
+              </motion.div>
+              <motion.div
+                className={cn("space-y-3 absolute top-0 left-0 right-0")}
+                animate={{ translateX: `${(1 - step) * 100}%` }}
+                transition={{
+                  ease: "easeInOut",
+                }}
+              >
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>비밀번호</FormLabel>
+                      <FormControl>
+                        <Input type={"password"} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>비밀번호 확인</FormLabel>
+                      <FormControl>
+                        <Input type={"password"} {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </motion.div>
+              <Button
+                type="button"
+                className={cn({ hidden: step === 1 })}
+                onClick={() => {
+                  form.trigger(["phone", "email", "username", "role"]);
+                  const phoneState = form.getFieldState("phone");
+                  const emailState = form.getFieldState("email");
+                  const usernameState = form.getFieldState("username");
+                  const roleState = form.getFieldState("role");
+                  if (!phoneState.isDirty || phoneState.invalid) return;
+                  if (!emailState.isDirty || emailState.invalid) return;
+                  if (!usernameState.isDirty || usernameState.invalid) return;
+                  if (!roleState.isDirty || roleState.invalid) return;
+
+                  setStep(1);
+                }}
+              >
+                다음 단계로 ⭢
+              </Button>
+              <Button className={cn({ hidden: step === 0 })}>계정 등록하기</Button>
+              <Button
+                type="button"
+                variant={"ghost"}
+                className={cn({ hidden: step === 0 })}
+                onClick={() => {
+                  setStep(0);
+                }}
+              >
+                이전 단계로
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
